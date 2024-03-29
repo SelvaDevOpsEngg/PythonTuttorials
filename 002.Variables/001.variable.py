0a93c84ea06ffc6d509c87e08260418a1f74264c
SelvaDevOpsEngg <selvarajdevops@gmail.com>
2024-03-29 13:27:57 +0530

    Updated

---
+++ 002.Variables/001.variable.py
@@ -0,0 +1,28 @@
+# Variables
+# Rules:
+# -------
+# It should start with alphabet or underscore(_)
+# It should not start with numerical.
+# It can be Alphnumeric.
+# It is case sensitve.
+# No keywords should be used.
+
+# Assigning a value to a variable.
+a=10
+print(a)
+
+_num=101
+print(_num)
+
+# Assign a value to a variables
+a=10 
+b=20
+print(a+b)
+
+x,y=10,20
+print(x+y)
+
+x=y=z=5
+print(x)
+print(y)
+print(z)
