637de2a225b755f03f73e10342aba02759867147
SelvaDevOpsEngg <selvarajdevops@gmail.com>
2024-03-30 13:47:47 +0530

    New file added

---
+++ 003.DataTypes/001.datatypes.py
@@ -0,0 +1,21 @@
+# Example:
+name = "Selvaraj"
+roll = "DevOps Engineer"
+salary = 12232344546
+
+# input() function:
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+salary = float(input("Enter your salary:"))
+print(name)
+print(age)
+print(salary)
+print(type(name))
+print(type(age))
+print(type(salary))
+
+# datatype conversion
+num1 = int(input("Enter a number"))
+print(num1)
+type(num1)
+print(num1+2)
